970bb71999720f8175127facfddf9c80d837e0a5
Kanzari <amine.kanzari@ett.tn>
2020-03-08 20:47:20 +0100

    Updates

---
+++ precache-manifest.4fbb45a87e42ed755ba68768db00a6b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b466aeeb90628528b25ba0fbcce79f",
+    "url": "/react-dropzone/index.html"
+  },
+  {
+    "revision": "45d68ae4fe20d29060ce",
+    "url": "/react-dropzone/static/css/main.b5373b26.chunk.css"
+  },
+  {
+    "revision": "3fc02c969e6b8be4133e",
+    "url": "/react-dropzone/static/js/2.714bf3a4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-dropzone/static/js/2.714bf3a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d68ae4fe20d29060ce",
+    "url": "/react-dropzone/static/js/main.33c5f965.chunk.js"
+  },
+  {
+    "revision": "da17cbd4d0d1adffb7d4",
+    "url": "/react-dropzone/static/js/runtime-main.73eef4b9.js"
+  }
+]);
